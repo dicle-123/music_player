232eaa10134046e35a2432f15170bade7430a1ce
dicle <diclealgemi66@gmail.com>
2023-03-31 14:34:52 +0300

    control keys actived

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const container = document.querySelector(".container");
 const image = document.querySelector("#music-image");
 const title = document.querySelector("#music-details .title");
 const singer = document.querySelector("#music-details .singer");
-const prev = document.querySelector("#controls #prev");
 const play = document.querySelector("#controls #play");
+const prev = document.querySelector("#controls #prev");
 const next = document.querySelector("#controls #next");
 
 const player = new MusicPlayer(musicList);
@@ -20,15 +20,41 @@ function displayMusic(music) {
     audio.src = "mp3/" + music.file;
 }
 
+play.addEventListener("click", () => {
+    const isMusicPlay = container.classList.contains("playing");
+    isMusicPlay ? pauseMusic() : playMusic();
+});
 
-play.addEventListener('click', () =>{
+prev.addEventListener("click", () => {
+    prevMusic();
+});
+
+next.addEventListener("click", () => {
+    nextMusic();
+});
+
+function prevMusic() {
+    player.prev();
+    let music = player.getMusic();
+    displayMusic(music);
+    playMusic();
+}
+
+function nextMusic() {
+    player.next();
+    let music = player.getMusic();
+    displayMusic(music);
+    playMusic();
+}
+
+function pauseMusic() {
+    container.classList.remove("playing");
+    play.classList = "fa-solid fa-play";
+    audio.pause();
+}
+
+function playMusic() {
+    container.classList.add("playing");
+    play.classList = "fa-solid fa-pause";
     audio.play();
-})
-
-
-
-
-
-
-
-
+}
--- musicplayer.js
+++ musicplayer.js
@@ -1,29 +1,27 @@
-class  MusicPlayer{
-    constructor(musicList){
-        this.musicList=musicList;
-        this.index=0;
+class MusicPlayer {
+    constructor(musicList) {
+        this.musicList = musicList;
+        this.index = 1;
     }
 
-    getMusic(){
+    getMusic() {
         return this.musicList[this.index];
     }
 
-    next(){
-     if(this.index + 1 != this.index){
-        this.index++;
-     }
-     else{
-        this.index=0;
-     }
-
+    next() {
+        if(this.index + 1 != this.musicList.length) {
+            this.index++;
+        }
+        else {
+             this.index = 0;
+        }
     }
 
-    previous(){
-        if(this.index != 0){
+    prev() {
+        if(this.index != 0) {
             this.index--;
-        }
-        else{
-            this.index=this.musicList.length-1;
+        } else {
+            this.index = this.musicList.length - 1;
         }
     }
 }
