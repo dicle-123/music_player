dde93a61d1c59985ad0d626b5ce1808daf838cf3
dicle <diclealgemi66@gmail.com>
2023-04-02 11:02:41 +0300

    dynamically generated playlist

--- app.js
+++ app.js
@@ -10,12 +10,14 @@ const duration=document.querySelector('#duration');
 const currentTime=document.querySelector('#current-time');
 const volume=document.querySelector('#volume');
 const volumeBar=document.querySelector('#volume-bar');
+const ul=document.querySelector('ul');
 
 const player = new MusicPlayer(musicList);
 
 window.addEventListener("load", () => {
     let music = player.getMusic();
     displayMusic(music);
+    displayMusicList(player.musicList);
 });
 
 function displayMusic(music) {
@@ -121,4 +123,26 @@ volume.addEventListener('click', () =>{
     }
 
     
-})
+});
+
+const displayMusicList = (list) =>{
+    for(let i=0; i<list.length; i++){
+        let liTag= `
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${list[i].getName()}</span>
+        <span id='music-${i}' class="badge bg-primary rounded-pill"></span>
+       <audio class='music-${i}' src='mp3/${list[i].file}'></audio>
+    </li>
+        `;
+
+        ul.insertAdjacentHTML('beforeend', liTag);
+        let liAudioDuration=ul.querySelector(`#music-${i}`);
+        let liAudioTag=ul.querySelector(`.music-${i}`);
+
+        liAudioTag.addEventListener('loadeddata', () =>{
+            liAudioDuration.innerText=calculateTime(liAudioTag.duration);
+        })
+
+   
+    }
+}
